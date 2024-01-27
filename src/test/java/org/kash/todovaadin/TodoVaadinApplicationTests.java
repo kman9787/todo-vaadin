a8a90bccf63a45061543e98be0dc61f022b16ecb
kancheta <kazancheta87@gmail.com>
2024-01-27 09:41:49 +0800

    Initial commit

---
+++ src/test/java/org/kash/todovaadin/TodoVaadinApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kash.todovaadin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoVaadinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
